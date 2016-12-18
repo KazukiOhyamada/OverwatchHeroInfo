16aea9f1a90da73baf48371c755be2bef3d4482d
Kazuki.ohymd <kazuki.ohyamada@gmail.com>
2016-12-18 19:51:51 +0900

    test

--- iOS/Main.cs
+++ iOS/Main.cs
@@ -14,6 +14,7 @@ namespace OverwatchHeroInfo.iOS
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
+			//2016/12/17
 			UIApplication.Main(args, null, "AppDelegate");
 		}
 	}
